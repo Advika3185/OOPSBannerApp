01bbac09ad6efb4450d516a9852c391375c0a9f0
Advika3185 <phoenixada2118@gmail.com>
2026-02-16 19:30:06 +0530

    UC4: Refactored to use String Array and Loop

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,12 +1,17 @@
 public class OOPSBanner {
     public static void main(String[] args) {
-        // UC2: Large OOPS Banner using 7-line format and 9-character width
-        System.out.println(String.join("","  ***** " , "   ***** " , "  ***** " , "  ***** "));
-        System.out.println(String.join(""," *     * " , " *     * " , " *    * " , " *     "));
-        System.out.println(String.join(""," *     * "  ," *     * " , " *    * " , " *      "));
-        System.out.println(String.join(""," *     * "  ," *     * " , " ***** " , "  ***** "));
-        System.out.println(String.join(""," *     * " , " *     * " , " * " , "          * "));
-        System.out.println(String.join(""," *     * " , " *     * " , " * " , "          * "));
-        System.out.println(String.join("","  ***** " , "   *****  "  , " * " , "      ***** "));
+        // UC4: Store lines in a String Array to eliminate repetitive code
+        String[] banner = new String[7];
+
+        banner[0] = String.join("","  ***** " , "   ***** " , "  ***** " , "  ***** ");
+        banner[1] = String.join(""," *     * " , " *     * " , " *    * " , " *     ");
+        banner[2] = String.join(""," *     * "  ," *     * " , " *    * " , " *      ");
+        banner[3] = String.join(""," *     * "  ," *     * " , " ***** " , "  ***** ");
+        banner[4] = String.join(""," *     * " , " *     * " , " * " , "          * ");
+        banner[5] = String.join(""," *     * " , " *     * " , " * " , "          * ");
+        banner[6] = String.join("","  ***** " , "   *****  "  , " * " , "      ***** ");
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
